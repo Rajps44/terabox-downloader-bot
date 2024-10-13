492ea0ed36e69c233fd96f2318820c2fcc89bbe3
Raj <singhayush88805@gmail.com>
2024-10-13 13:16:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,23 @@
-API_ID = 123456  # api id
-API_HASH = "ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # api hash
+API_ID = 25208218  # api id
+API_HASH = "cc8e841203c92f077015b865f44a8663"  # api hash
 
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
+BOT_TOKEN = "7478730845:AAELg_8E4AXbQnu6t6MiGtrAXG1dOxGpze8"  # bot token
 
 
 # REDIS
 HOST = "localhost"  # redis host uri
-PORT = 6379  # redis port
+PORT = 8080  # redis port
 PASSWORD = ""  # redis password
 
-PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
+PRIVATE_CHAT_ID = -1002192763590  # CHAT WHERE YOU WANT TO STORE VIDEOS
 # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=;
-COOKIE = ""
+COOKIE = "Secure-3PAPISID=JBqb1gGQAZPLIOWm/AkwZIxbDHNhFp70aq; Secure-3PSID=g.a000mQiufn_yKsl0Qec-w5DNWvX1dfhQKLPF4lTGDh4Xx3boj9OQ2dkClARQZHNpjbsdfpR66QACgYKAYMSARUSFQHGX2Mi7Sv74VcuPDaU2v4zCRmvnhoVAUF8yKpzi1Cz-c8ZC9nU8i7iwwgY0076; NID=516=bD7883VKHvrabQKRYnn2GObm8p5Uk0k48p4co3ecTzMU0cQ2UI4AOlB3CTkZFEucR8Dp-3OJjsx9WfzNW9OtaSTzEmynHx_nVrcaor_v-aE3LiySInFZKb5hXxRfjErKvqadaNWy41z2EfsloXFrey5q6f2RZTKRQk2cL8AOYDqevZS9WYKXpwJvlYDMlvdZduidtAyD9tmKsK52efQx4Z5EAgUL8MRjVQZtx4rKqJZ1jCWE_WVqAdsWpbJouT2yeSY2SACj2TuRz6fwyo5skhoS8IDXZtsolS0RIAE; __Secure-3PSIDCC=AKEyXzUYJdr2YK4Cqz_Oj0Far88ylqc1FzZ0mZly68xyYl-Wap5UNrg6n5q270KdctaXcKfPsQ"
 ADMINS = [1317173146]
 
 
-BOT_USERNAME = "teraboxdown_bot"
+BOT_USERNAME = "Netflixmovielakh_bot"
 
 # Force user to join this channel. (make sure you have promoted the bot on this chat.)
-FORCE_LINK = "@RoldexVerse"
+FORCE_LINK = "@n_flixmovie"
 
 PUBLIC_EARN_API = ""  # https://publicearn.com/api
